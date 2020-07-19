b6e179bea02053adc25b4a27a466b2e9d485f6fa
Sagun Singh <sagun.singh.vt@gmail.com>
2020-07-19 19:39:33 -0400

    Clean up `src/geometry.c`
    Add documentation and fix spacing and formatting in `geometry.c`.

--- src/geometry.c
+++ src/geometry.c
@@ -10,8 +10,8 @@
 
 struct Mesh geodesic_icosahedron(int subdivisions) {
 
-  #define ICOSAHEDRON_FACES(level) ((unsigned int) (20 * powf(4, level)))
-  #define ICOSAHEDRON_EDGES(level) ((unsigned int) (30 * powf(4, level)))
+  #define ICOSAHEDRON_FACES(level)    ((unsigned int) (20 * powf(4, level)))
+  #define ICOSAHEDRON_EDGES(level)    ((unsigned int) (30 * powf(4, level)))
   #define ICOSAHEDRON_VERTICES(level) ((unsigned int) (2 + (10 * powf(4, level))))
 
   struct Mesh icosahedron;
@@ -33,54 +33,67 @@ struct Mesh geodesic_icosahedron(int subdivisions) {
      { 0.276385,  0.447215, -0.850640},
      { 0.894425,  0.447215,  0.000000},
      { 0.000000,  1.000000,  0.000000}};
-
   unsigned int original_faces[20][3] =
-    {{1,  0,  5},
-     {0,  1,  2},
-     {0,  2,  3},
-     {0,  3,  4},
-     {0,  4,  5},
-     {1,  5,  10},
-     {2,  1,  6},
-     {3,  2,  7},
-     {4,  3,  8},
-     {5,  4,  9},
-     {1,  10, 6},
-     {2,  6,  7},
-     {3,  7,  8},
-     {4,  8,  9},
-     {5,  9, 10},
+    {{1,   0,  5},
+     {0,   1,  2},
+     {0,   2,  3},
+     {0,   3,  4},
+     {0,   4,  5},
+     {1,   5, 10},
+     {2,   1,  6},
+     {3,   2,  7},
+     {4,   3,  8},
+     {5,   4,  9},
+     {1,  10,  6},
+     {2,   6,  7},
+     {3,   7,  8},
+     {4,   8,  9},
+     {5,   9, 10},
      {6,  10, 11},
-     {7,  6,  11},
-     {8,  7,  11},
-     {9, 8,  11},
-     {10, 9, 11}};
+     {7,   6, 11},
+     {8,   7, 11},
+     {9,   8, 11},
+     {10,  9, 11}};
 
+  // Alternating buffers to store intermediate subdivision data.
   float vertices_1[icosahedron.vertices_len][3];
   float vertices_2[icosahedron.vertices_len][3];
   unsigned int faces_1[icosahedron.faces_len][3];
   unsigned int faces_2[icosahedron.faces_len][3];
 
+  // Begin with the original data in the first buffers.
   memcpy(vertices_1, &original_vertices, sizeof(original_vertices));
   memcpy(faces_1, &original_faces, sizeof(original_faces));
 
+  // References to present and next buffer.
   float (*vertices_from)[icosahedron.vertices_len][3] = &vertices_1;
   float (*vertices_to)[icosahedron.vertices_len][3] = &vertices_2;
   unsigned int (*faces_from)[icosahedron.faces_len][3] = &faces_1;
   unsigned int (*faces_to)[icosahedron.faces_len][3] = &faces_2;
 
+  // For arbitrarily adding to the next buffer.
   int vertices_to_length = 0;
   int faces_to_length = 0;
 
+  // A map of two faces to their respective dynamically generated edge.
   int edge_map_length = 0;
   unsigned int edge_map[ICOSAHEDRON_EDGES(subdivisions)][3];
 
+  // Add a new vertex to the next buffer.
   #define vertex(x, y, z) \
     (*vertices_to)[vertices_to_length][0] = x; \
     (*vertices_to)[vertices_to_length][1] = y; \
     (*vertices_to)[vertices_to_length][2] = z; \
     vertices_to_length++; \
 
+  // Add an old vertex to the next buffer.
+  #define old_vertex(v) \
+    vertex((*vertices_from)[v][0], \
+           (*vertices_from)[v][1], \
+           (*vertices_from)[v][2]); \
+
+  // Add a new vertex whose direction is the same as a midpoint
+  // and whose vector length is 1
   #define midpoint(from, to) \
     float x = ((*vertices_from)[from][0] + (*vertices_from)[to][0]) / 2; \
     float y = ((*vertices_from)[from][1] + (*vertices_from)[to][1]) / 2; \
@@ -88,13 +101,19 @@ struct Mesh geodesic_icosahedron(int subdivisions) {
     float length = sqrtf(powf(x, 2) + powf(y, 2) + powf(z, 2)); \
     vertex(x / length, y / length, z / length);
 
+  // Add a face to the next buffer.
+  #define face(p1, p2, p3) \
+    (*faces_to)[faces_to_length][0] = p1; \
+    (*faces_to)[faces_to_length][1] = p2; \
+    (*faces_to)[faces_to_length][2] = p3; \
+    faces_to_length++; \
+
+  // Retrieve, and create if necessary, an edge from the edge map.
   #define edge(from, to, store) \
     store = -1; \
     for (int s = 0; s < edge_map_length; s++) { \
-      if ((edge_map[s][0] == from && \
-           edge_map[s][1] == to) || \
-          (edge_map[s][0] == to && \
-           edge_map[s][1] == from)) { \
+      if ((edge_map[s][0] == from && edge_map[s][1] == to)    || \
+          (edge_map[s][0] == to   && edge_map[s][1] == from)) { \
         store = edge_map[s][2]; \
         edge_map[s][0] = edge_map[edge_map_length-1][0]; \
         edge_map[s][1] = edge_map[edge_map_length-1][1]; \
@@ -111,20 +130,11 @@ struct Mesh geodesic_icosahedron(int subdivisions) {
       edge_map_length++; \
     } \
 
-  #define face(p1, p2, p3) \
-    (*faces_to)[faces_to_length][0] = p1; \
-    (*faces_to)[faces_to_length][1] = p2; \
-    (*faces_to)[faces_to_length][2] = p3; \
-    faces_to_length++; \
-
-  #define old_vertex(v) \
-    vertex((*vertices_from)[v][0], \
-           (*vertices_from)[v][1], \
-           (*vertices_from)[v][2]); \
-
+  // Perform the icosahedron subdivision.
   for (int i = 0; i < subdivisions; i++) {
     for (int v = 0; v < ICOSAHEDRON_VERTICES(i); v++) {
-        vertex((*vertices_from)[v][0], (*vertices_from)[v][1],
+        vertex((*vertices_from)[v][0],
+               (*vertices_from)[v][1],
                (*vertices_from)[v][2]);
     }
     for (int f = 0; f < ICOSAHEDRON_FACES(i); f++) {
@@ -138,6 +148,7 @@ struct Mesh geodesic_icosahedron(int subdivisions) {
       face(mid3, (*faces_from)[f][0], mid1);
     }
 
+    // Switch the buffer references and reset associated variables.
     float (*vertices_temp)[icosahedron.vertices_len][3] = vertices_from;
     unsigned int (*faces_temp)[icosahedron.faces_len][3] = faces_from;
     vertices_from = vertices_to;
